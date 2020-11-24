c534319bc24660ab348c283fc2e81e2dc4fde184
presley m <muwanpresley@gmail.com>
2020-11-24 00:28:47 -0500

    added db config

--- chit_chat_api/chit_chat_api/settings.py
+++ chit_chat_api/chit_chat_api/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'chit_chat_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'changit',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'OPTIONS' : {
+                'options': '-c search_path=messenger,public'
+            }
     }
 }
 
